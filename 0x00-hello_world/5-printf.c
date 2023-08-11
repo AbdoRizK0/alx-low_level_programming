c273116dee47a71e5bea61944dec509fab6abf82
f <bidobibo5@gmail.com>
2023-08-11 05:40:02 +0300

    L

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+/**
+ * main - Entry point
+ *
+ * Description: gg well played
+ *
+ * Return : always 0
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
