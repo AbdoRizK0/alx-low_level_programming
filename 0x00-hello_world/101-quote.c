791288808ee49d05a71f98c015f9ca1b9e6fa8d1
f <bidobibo5@gmail.com>
2023-08-11 05:33:21 -0700

    f

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,11 @@
 #include <unistd.h>
  /**
-  * main - this is main function
-  * Description: write (int fd.const void *buf.size_t count)
-  * Return: always 1
+  * main - main function
+  * Return: always 0
   */
 
 int main(void)
-{	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-	write(1, quo, 59);
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
