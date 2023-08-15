6be75e88e32e6353dbfbc9e9b0db77ed9282e7aa
f <bidobibo5@gmail.com>
2023-08-15 14:57:29 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <main.h>
+
+/*
+ * main - sloving
+ *
+ * Description: printing
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+	char pmeat[] = "_putchar";
+	int ch;
+		printf("%s\n", pmeat);
+
+	return (0);
+}
