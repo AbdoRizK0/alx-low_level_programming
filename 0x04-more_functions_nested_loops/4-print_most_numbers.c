38f6867786b9b311f7f92d6e23526905b5a23ca3
AbdoRizK0 <bidobibo5@gmail.com>
2023-11-05 18:03:31 +0200

    new solu

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,17 +8,17 @@
  * Return: Always 0 (Success)
  *
  *
-*/
+ */
 
 void print_most_numbers(void)
 {
-	int num;
-
-	for (num = 0; num <= 9; num++)
+	int num = 0;
+	while (num <= 9)
 	{
-		if (num == 2 || num == 4)
-			continue;
-		_putchar(num - 48);
+		if (num != 2 && num != 4)
+
+			_putchar(num + 48);
+		num++;
 	}
 	_putchar('\n');
 }
